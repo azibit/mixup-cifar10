fc76c9e8077d776cf8c7ca5489cfc1e38c6c5032
azibit <abdulazeezidris28@gmail.com>
2021-11-09 16:00:40 -0600

    Update train.py

--- train.py
+++ train.py
@@ -4,6 +4,9 @@
 #
 # This source code is licensed under the license found in the LICENSE file in
 # the root directory of this source tree.
+
+#train.py --lr=0.1 --seed=20170922 --decay=1e-4 --epoch=2 --trials=2 --dataset_dir=../Datasets -b for baseline
+#train.py --lr=0.1 --seed=20170922 --decay=1e-4 --epoch=2 --trials=2 --dataset_dir=../Datasets for mixup
 from __future__ import print_function
 
 import argparse
