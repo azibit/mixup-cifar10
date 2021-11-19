d803fc92bc6b0d7d5e77ca2e997a486adaf48a0a
azibit <abdulazeezidris28@gmail.com>
2021-11-19 09:24:05 -0600

    Update train.py file with code to allow saving checkpoint for each trial

--- train.py
+++ train.py
@@ -9,9 +9,7 @@
 #train.py --lr=0.1 --seed=20170922 --decay=1e-4 --epoch=2 --trials=2 --dataset_dir=../Datasets --iterations 2 --image_size 32 for mixup
 from __future__ import print_function
 
-import argparse
-import csv
-import os, sys, glob
+import argparse, csv, os, sys, glob
 
 import numpy as np
 import torch
@@ -23,6 +21,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
 import models
+import torchvision.models as model
 from utils import progress_bar, make_prediction
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -33,7 +32,7 @@ parser.add_argument('--model', default="ResNet18", type=str,
                     help='model type (default: ResNet18)')
 parser.add_argument('--name', default='0', type=str, help='name of run')
 parser.add_argument('--seed', default=0, type=int, help='random seed')
-parser.add_argument('--batch-size', default=128, type=int, help='batch size')
+parser.add_argument('--batch-size', default=16, type=int, help='batch size')
 parser.add_argument('--epoch', default=200, type=int,
                     help='total epochs to run')
 parser.add_argument('--no-augment', dest='augment', action='store_false',
@@ -55,15 +54,9 @@ args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
 
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
 torch.manual_seed(123)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(123)
-
-if not os.path.exists(args.dataset_dir):
-    file_utils.create_dir(args.dataset_dir)
 
 dataset_list = sorted(glob.glob(args.dataset_dir + "/*"))
 print("Dataset List: ", dataset_list)
@@ -113,10 +106,12 @@ def mixup_data(x, y, alpha=1.0, use_cuda=True):
     y_a, y_b = y, y[index]
     return mixed_x, y_a, y_b, lam
 
+# Idea is to include the original dataset while training with mixup so as to add more data to the training
+def mixup_criterion_v1(criterion, pred, y_a, y_b, lam, x, y):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b) + criterion(x, y)
 
 def mixup_criterion(criterion, pred, y_a, y_b, lam):
     return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
-
 
 def train(epoch):
     print('\nEpoch: %d' % epoch)
@@ -150,9 +145,9 @@ def train(epoch):
             correct += (lam * predicted.eq(targets_a.data).cpu().sum().float()
                         + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        optimizer.zero_grad() # Zeroes out the gradients from previous passes if any
+        loss.backward() # Computes the gradient values based on calculus
+        optimizer.step() # Update variables with gradient values
 
         progress_bar(batch_idx, len(trainloader),
                      'Loss: %.3f | Reg: %.5f | Acc: %.3f%% (%d/%d)'
@@ -161,7 +156,7 @@ def train(epoch):
     return (train_loss/batch_idx, reg_loss/batch_idx, 100.*correct/total)
 
 
-def test(epoch, loader):
+def test(epoch, loader, direct_for_checkpoint):
     global best_acc
     net.eval()
     test_loss = 0
@@ -184,13 +179,13 @@ def test(epoch, loader):
                         correct, total))
     acc = 100.*correct/total
     if acc > best_acc:
-        checkpoint(acc, epoch)
-    if acc > best_acc:
+        checkpoint(acc, epoch, direct_for_checkpoint)
         best_acc = acc
+
     return (test_loss/batch_idx, 100.*correct/total)
 
 
-def checkpoint(acc, epoch):
+def checkpoint(acc, epoch, direct_for_checkpoint):
     # Save checkpoint.
     print('Saving..')
     state = {
@@ -199,9 +194,9 @@ def checkpoint(acc, epoch):
         'epoch': epoch,
         'rng_state': torch.get_rng_state()
     }
-    if not os.path.isdir('checkpoint'):
-        os.mkdir('checkpoint')
-    torch.save(state, './checkpoint/ckpt.t7' + args.name + '_'
+    if not os.path.isdir(direct_for_checkpoint):
+        os.mkdir(direct_for_checkpoint)
+    torch.save(state, f'./{direct_for_checkpoint}/ckpt.t7' + args.name + '_'
                + str(args.seed))
 
 
@@ -226,29 +221,36 @@ for dataset in dataset_list:
 
             print("Iteration", iteration, " Experiment: ", trial, "for dataset", dataset)
 
+            # Location to save checkpoint
+            current_exp = "_ite_" + str(iteration) + "_" + str(trial) + "_dataset_" + dataset
+            direct_for_checkpoint = 'checkpoint' + current_exp
+
+            best_acc = 0  # best test accuracy
+            start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
             trainset = datasets.ImageFolder(os.path.join(dataset, 'train'),
                                                       transform_train)
             trainloader = torch.utils.data.DataLoader(trainset,
                                                       batch_size=args.batch_size,
-                                                      shuffle=True, num_workers=8)
+                                                      shuffle=True, num_workers=2)
 
             testset = datasets.ImageFolder(os.path.join(dataset, 'test'),
                                                       transform_test)
-            testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-                                                     shuffle=False, num_workers=8)
+            testloader = torch.utils.data.DataLoader(testset, batch_size=8,
+                                                     shuffle=False, num_workers=2)
 
             # Model
             if args.resume:
                 # Load checkpoint.
                 print('==> Resuming from checkpoint..')
-                assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-                checkpoint = torch.load('./checkpoint/ckpt.t7' + args.name + '_'
+                assert os.path.isdir(direct_for_checkpoint), 'Error: no checkpoint directory found!'
+                checkpoint = torch.load(f'./{direct_for_checkpoint}/ckpt.t7' + args.name + '_'
                                         + str(args.seed))
                 net = checkpoint['net']
                 best_acc = checkpoint['acc']
                 start_epoch = checkpoint['epoch'] + 1
                 rng_state = checkpoint['rng_state']
-                torch.set_rng_state(rng_state)
+                torch.set_rng_state(rng_state) # Set the random number generator state
             else:
                 print('==> Building model..')
 
@@ -256,15 +258,14 @@ for dataset in dataset_list:
                 if args.image_size == 32:
                     net = models.__dict__[args.model](num_classes=len(testset.classes))
                 else:
-                    net = models.densenet161()
+                    net = model.densenet161()
                     net.classifier = nn.Linear(net.classifier.in_features, len(testset.classes))
 
             results = "results_" + str(trial)
             if not os.path.isdir(results):
                 os.mkdir(results)
-            logname = (results + '/log_' + net.__class__.__name__ + '_' + args.name + '_'
+            logname = (results + '/log_' + current_exp + '_' + net.__class__.__name__ + '_' + args.name + '_'
                        + str(args.seed) + '.csv')
-
 
             if not os.path.exists(logname):
                 with open(logname, 'w') as logfile:
@@ -283,9 +284,10 @@ for dataset in dataset_list:
             optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9,
                                   weight_decay=args.decay)
 
+
             for epoch in range(start_epoch, args.epoch):
                 train_loss, reg_loss, train_acc = train(epoch)
-                test_loss, test_acc = test(epoch, testloader)
+                test_loss, test_acc = test(epoch, testloader, direct_for_checkpoint)
 
                 adjust_learning_rate(optimizer, epoch)
                 with open(logname, 'a') as logfile:
@@ -295,8 +297,8 @@ for dataset in dataset_list:
 
                 if epoch + 1 == args.epoch:
                     with open(current_dataset_file, 'a') as f:
-                        checkpoint = torch.load('./checkpoint/ckpt.t7' + args.name + '_'
+                        checkpoint_result = torch.load(f'./{direct_for_checkpoint}/ckpt.t7' + args.name + '_'
                                                 + str(args.seed))
-                        net = checkpoint['net']
+                        net = checkpoint_result['net']
                         print("Test result for iteration", iteration, "experiment:", trial, " for dataset ", dataset, file = f)
                         print(make_prediction(net, testset.classes, testloader, 'save'), file = f)
